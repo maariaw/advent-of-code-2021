f54e9cb9828b6e29a3423f56ade74d8c50b5c25a
Maaria Wahlström <65168298+maariaw@users.noreply.github.com>
2021-12-08 01:29:43 +0200

    Orchestrate a crab attack

---
+++ Day07/puzzle2/src/index.py
@@ -0,0 +1,30 @@
+from inputreader import InputReader
+
+def main():
+    path = "/home/mawahlst/Documents/Projects/AdventOfCode/Day07/input.txt"
+    reader = InputReader(path)
+    crab_positions = reader.get_one_line_ints()
+    # print(crab_positions)
+
+    fuel_costs = {}
+
+    low = min(crab_positions)
+    high = max(crab_positions)
+
+    for position in range(low, high + 1):
+        fuel_sum = 0
+        for crab in crab_positions:
+            distance = abs(position - crab)
+            fuel_cost = (distance / 2) * (distance + 1)
+            fuel_sum += fuel_cost
+        fuel_costs[position] = fuel_sum
+    
+    optimal_position = min(fuel_costs.keys(), key=(lambda k: fuel_costs[k]))
+    optimal_cost = fuel_costs[optimal_position]
+
+
+    print(f"The optimal position to align to is {optimal_position}")
+    print(f"This will cost a total of {optimal_cost} fuel")
+
+if __name__ == "__main__":
+    main()
